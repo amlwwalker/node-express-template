3f6e8f6cb5c531b77d1895e91bc44f8b8d3091f6
Alex <alex@decoded.co>
2016-05-02 18:39:48 +0100

    chaning port

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ echo "current commit: $hash"
 echo "installing npm libraries"
 docker run -v $(pwd):/srv/app -w /srv/app iron/node:dev npm install
 echo "starting node app"
-docker run --rm --name template -p 3000:3000 -v $(pwd):/srv/app -w /srv/app iron/node node app.js
+docker run --rm --name template -p 3030:3000 -v $(pwd):/srv/app -w /srv/app iron/node node app.js
